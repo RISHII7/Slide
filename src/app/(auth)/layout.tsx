8771a2c8e0592ffdf37c2324b4d54f2ea61383bc
RISHII7 <rishikesh.palande07@gmail.com>
2024-12-06 11:50:12 +0530

    Clerk Authentication Setup

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+type Props = {
+    children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            {children}
+        </div>
+    );
+};
+export default Layout
