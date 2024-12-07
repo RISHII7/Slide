50041e0804cd1cc60279b62c02483f27858dd8e9
RISHII7 <rishikesh.palande07@gmail.com>
2024-12-07 16:40:36 +0530

    Automation Page

---
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -0,0 +1,12 @@
+// WIP: Set some metadata
+
+const AutomationIdPage = () => {
+    // WIP: Prefetch user automation data
+
+    return (
+        <div className="flex flex-col items-center gap-y-20">
+            
+        </div>
+    );
+};
+export default AutomationIdPage;
